6de12e532b2b45a403b32b8f12662f557e9620ff
Daniel <daniel.ramos.rodrig@gmail.com>
2025-11-01 16:30:29 +0100

    fix: Move to profile open/close GUI hooks.

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import asyncio
-from aqt import mw
+from aqt import mw, gui_hooks
 from aqt.operations import QueryOp
 from aqt.theme import theme_manager
 
@@ -11,6 +11,8 @@ vendor_dir = os.path.join(os.path.dirname(__file__), "vendor")
 sys.path.insert(0, vendor_dir)
 
 from dbus_fast.aio import MessageBus
+
+profile_close_event = None
 
 async def bus_listener():
 
@@ -36,16 +38,21 @@ async def bus_listener():
 
     settings.on_setting_changed(callback)
 
-    await asyncio.Event().wait()
+    await profile_close_event.wait()
 
-def on_success():
-    pass
+def on_profile_open():
+    profile_close_event = asyncio.Event()
+    dbus_op = QueryOp(
+        parent=mw,
+        op=lambda _: asyncio.run(bus_listener()),
+        success=on_success    # Don't do anything on listener exit.
+    )
+    dbus_op.without_collection().run_in_background()
 
-dbus_op = QueryOp(
-    parent=mw,
-    op=lambda _: asyncio.run(bus_listener()),
-    success=on_success    # Don't do anything on listener exit.
-)
+def on_profile_close():
+    # Tell the asyncio event loop to close.
+    profile_close_event.set()
 
-dbus_op.without_collection().run_in_background()
+gui_hooks.profile_will_open(on_profile_open)
+gui_hooks.profile_will_close(on_profile_close)
 
